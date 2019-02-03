0cb8948a9d68838c7fea1d5fe1e66c8dfa3f6516
guozhaoxin <1345616978@qq.com>
2019-02-03 17:55:56 +0800

    20190203 by gold finish num 29

--- src/num001_100/num021_030/num029/num029.java
+++ src/num001_100/num021_030/num029/num029.java
@@ -23,11 +23,59 @@ The divisor will never be 0.
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.
  */
 
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+
 public class num029 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+//        System.out.println(s.divide(0,1));
+        System.out.println(s.divide(7,-3));
+        System.out.println(s.divide(-2147483648,-1));
+    }
 }
 
 class Solution {
     public int divide(int dividend, int divisor) {
-        return 0;
+        if(divisor == Integer.MIN_VALUE) {
+            if(dividend == Integer.MIN_VALUE) return 1;
+            else return 0;
+        }
+        if(dividend == 0){
+            return 0;
+        }
+        boolean positive = !((dividend > 0)^(divisor > 0));
+        long remain;
+        if(dividend == Integer.MIN_VALUE){
+            remain = 2147483648L;
+        }
+        else {
+            remain = Math.abs(dividend);
+        }
+        divisor = Math.abs(divisor);
+        long pos = 1,base = divisor;
+        while (base <= remain){
+            base <<= 1;
+            pos <<= 1;
+        }
+        long res = 0;
+        while (pos > 0){
+            if(base <= remain){
+                remain -= base;
+                res += pos;
+            }
+            pos >>= 1;
+            base >>= 1;
+        }
+        if(res == 2147483648L){
+            if(positive){
+                return Integer.MAX_VALUE;
+            }else {
+                return Integer.MIN_VALUE;
+            }
+        }
+        if(!positive){
+            res = -res;
+        }
+        return (int)res;
     }
 }
