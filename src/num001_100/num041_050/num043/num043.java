2defd992c41305d3f328b1287fe1de58b1f3c9ac
guozhaoxin <1345616978@qq.com>
2019-02-26 17:36:32 +0800

    20190221 finish num 43

--- src/num001_100/num041_050/num043/num043.java
+++ src/num001_100/num041_050/num043/num043.java
@@ -22,27 +22,52 @@ You must not use any built-in BigInteger library or convert the inputs to intege
 
 public class num043 {
     public static void main(String[] args) {
-        StringBuilder hah = new StringBuilder();
-        hah.append("a");
-        hah.append("b");
-        hah.reverse();
-        System.out.println(hah);
+        Solution s = new Solution();
+        System.out.println(s.multiply("123","456"));
+        System.out.println(s.multiply("7","12"));
+        System.out.println(s.multiply("7","6"));
+        System.out.println(s.multiply("7","7"));
+        System.out.println(s.multiply("7","1"));
+        System.out.println(s.multiply("1","7"));
+        System.out.println(s.multiply("1","1"));
+        System.out.println(s.multiply("0","123"));
+        System.out.println(s.multiply("1234","0"));
     }
 
 }
 
 class Solution {
     public String multiply(String num1, String num2) {
-        StringBuilder res = new StringBuilder();
-        res.reverse();
-        int mul,temp;
-        for(int out = num1.length() - 1;out >= 0;out--){
-            int carry = 0;
-            for(int in = num1.length() - 1;in >= 0;in--){
-                temp = (num1.charAt(out) - '0') * (num2.charAt(in) - '0') + carry;
-
-            }
+        if(num1.equals("0") || num2.equals("0")){
+            return "0";
         }
-        return res.toString();
+        if(num1.equals("1")){
+            return num2;
+        }
+        if(num2.equals("1")){
+            return num1;
+        }
+        int lengthen = num1.length() + num2.length();
+        StringBuilder res = new StringBuilder(lengthen);
+        for(int i = 0; i < lengthen;i++){
+            res.append('0');
+        }
+        int resBit = 0;
+        for(int n1_index = num1.length() - 1;n1_index >= 0;n1_index--){
+            int temp,carry = 0,cur = resBit;
+            for(int n2_index = num2.length() - 1;n2_index >= 0;n2_index--,cur++){
+                temp = (num1.charAt(n1_index) - '0') * (num2.charAt(n2_index) - '0') + carry + res.charAt(cur) - '0';
+                carry = temp / 10;
+                res.setCharAt(cur,(char)('0' + temp % 10));
+            }
+            if(carry != 0) {
+                res.setCharAt(cur, (char) (carry + res.charAt(cur)));
+            }
+            resBit++;
+        }
+        while (res.charAt(res.length() - 1) == '0'){
+            res.deleteCharAt(res.length() - 1);
+        }
+        return res.reverse().toString();
     }
 }
